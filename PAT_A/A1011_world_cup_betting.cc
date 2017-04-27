cfa494ec09616d6a3b91d8d6230566e2808d0433
qiongqiong520 <1153478519@qq.com>
2017-04-27 20:01:12 +0800

    onlyone

---
+++ PAT_A/A1011_world_cup_betting.cc
@@ -0,0 +1,18 @@
+ ///
+ /// @file    A1011_world_cup_betting.cc
+ /// @author  lemon(haohb13@gmail.com)
+ /// @date    2017-04-26 12:51:57
+ ///
+#include<cstdio>
+
+char s[3]={'W','T','L'};
+int main()
+{
+	double ans =1.0,tmp,a;
+	for(int i=0;i<3;i++)
+	{
+		tmp =0.0;
+		for(int i =0;i<3;j++)
+		{
+			scanf("%lf",&a);
+
